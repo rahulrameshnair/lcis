ef2e4564024cd6e54f314cf5cbdb625c5c83f44d
Nair, Rahul Ramesh <rahul.nair@dlr.de>
2023-10-24 20:00:55 +0200

    revision
    revised the whole folder structure
    added gitignore
    added inventory module
    removed redundant and old scripts
    general housekeeping

--- LCI visualization/readme.md
+++
@@ -1,3 +0,0 @@
-#Under construction
-
-Do not use in production environments. Check back later for more updates.
--- inventory/network/libs/print_lci.py
+++ inventory/network/libs/print_lci.py
@@ -1,25 +1,35 @@
 import brightway2 as bw
 import bw2data as bd
 
-'''
-Documentation
-1. print_lci_all is used to output all the activities and the corresponding exchanges from a selected brightway database. The printing is done in dictionary format with all the details such as the linked database names, activity codes and so on.
+"""
+Used for printing all the activities and their corresponding exchanges in a database according to the way in which
+they are stored within the brightway database.
+"""
+def print_all(selected_database):
+    """
+    To output all the activities and the corresponding exchanges from a selected brightway database. 
+    The printing is done in dictionary format with all the details such as the linked database names, activity codes and so on.
 
-2. print_lci_classify is used to classify and count those exchanges and activities inherent/intrinsic to the database and those that copied/imported from other databases. This is because, in BW, copied activities have their exchanges stored in a dictionary format while intrinsic activities have their exchanges stored in dictionary form. Within each such activities, the number of technosphere flows and biosphere flows are to be identified.
-    There is another type of flow called "downstream consumers" in the activity browser. This just means that if the product from one of the activity (a1, say) is used as a technosphere input for the activity (a2, say). Then, a2 becomes the downstream consumer of a1. This is not classified here because it is a just an interdependency.
-
-'''
-def print_lci_all(selected_database):
+    """
     for activity in selected_database:
         print ('Activity:', activity.as_dict(), '\n')
         for exchange in activity.exchanges():
             print('\t','Exchange', exchange.as_dict(), '\n')
         print('\n')
 
-def print_lci_classify(selected_database):
+def print_classified(selected_database):
+    """
+    To classify and count those exchanges and activities intrinsic to the database and those that copied/imported from other databases. 
+    This is because, in BW, copied activities have their exchanges stored in a dictionary format while intrinsic activities have their exchanges stored in dictionary form. 
+    Within each such activities, the number of technosphere flows and biosphere flows are to be identified.
+        There is another type of flow called "downstream consumers" in the activity browser. This just means that if the product from one of the activity (a1, say) is used as a technosphere input for the activity (a2, say). 
+        Then, a2 becomes the downstream consumer of a1. This is not classified here because it is a just an interdependency.
+
+    """
     imported_activity_counter = 0
     intrinsic_activity_counter = 0
     total_activity_counter = 0
+
     print ('The following activities are from the database:', selected_database, '\n')
     for activity in selected_database:
         #sets counters for technosphere, biosphere and product flows.
@@ -27,7 +37,7 @@ def print_lci_classify(selected_database):
         biosphere_counter = 0
         product_counter = 0
         total_activity_counter = total_activity_counter + 1
-        # intrinsic activities do not have the worksheet name in the dictionary containing the details of activity. This 'worksheet name' contains the name of the database from which this activity was copied from.
+        # intrinsic activities do not have the 'worksheet name' in the dictionary containing the details of activity. This 'worksheet name' contains the name of the database from which this activity was copied from.
         if 'worksheet name' not in activity:
           
             intrinsic_activity_counter = intrinsic_activity_counter + 1
@@ -42,8 +52,7 @@ def print_lci_classify(selected_database):
                     product_counter = product_counter + 1
                     
             
-            print ('\t', str(total_activity_counter)+'.',  activity, "Type: Intrinsic", " with", technosphere_counter, " technosphere flows", biosphere_counter, " biosphere flows", product_counter, " products",  '\n')
-            print('\t \t', 'This activity', activity['name'], 'relies on the databases:', name_database_dependecies, '\n')
+            print ('\t', str(total_activity_counter)+'.',  activity, "Type: Intrinsic activity", " with", technosphere_counter, " technosphere flows", biosphere_counter, " biosphere flows", product_counter, " products",  '\n')
         else:
             
             imported_activity_counter = imported_activity_counter + 1
@@ -58,7 +67,7 @@ def print_lci_classify(selected_database):
                     product_counter = product_counter + 1
                     if exchange.as_dict()['input'][0] not in name_database_dependecies:
                         name_database_dependecies.append(exchange.as_dict()['input'][0]) #to get the database from which this production exchange within the activity has been imported, if different from the database name of the imported activity
-            print ('\t', str(total_activity_counter)+'.', activity, "Type: Intrinsic", " with", technosphere_counter, " technosphere flows", biosphere_counter, " biosphere flows", product_counter, " products",  '\n')
+            print ('\t', str(total_activity_counter)+'.', activity, "Type: Imported activity", " with", technosphere_counter, " technosphere flows", biosphere_counter, " biosphere flows", product_counter, " products",  '\n')
     print ("In total, this database has ", imported_activity_counter, " imported activities and", intrinsic_activity_counter, " intrinsic activities")
 
 
